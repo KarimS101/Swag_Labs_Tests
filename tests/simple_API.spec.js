c683c149386759816a742435150c4872b3d40bd8
Karim Saidi <keirasaidi@Karims-MacBook-Pro.local>
2025-05-15 12:34:29 +0100

    Added negative scneario

--- tests/simple_API.spec.js
+++ tests/simple_API.spec.js
@@ -18,6 +18,13 @@ test('simple 200 test', async ({ request }) => {
     //Check the response length for the endpoint is 10
     expect(users.length).toBe(10)
 
+})
 
+//NEGATIVE SCENARIO
+test('Get 404 when searching for 999 users in response', async ({ request }) => {
+    const response = await request.get("https://jsonplaceholder.typicode.com/users/999")
+
+    //Check the status code is 200
+    expect(response.status()).toBe(404)
 
 })
