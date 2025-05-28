d4a51080e17af8bfe54d9b291808b9b6db7f030a
Karim Saidi <keirasaidi@Karims-MacBook-Pro.local>
2025-05-28 22:17:52 +0100

    Cleaned up Code in Test.js to make it more readable and reusable

--- tests/allstate_Tests.spec.js
+++ tests/allstate_Tests.spec.js
@@ -6,128 +6,68 @@ import creditNoHitPage from '../Pages/creditNoHit';
 import myAccountHomePage from '../Pages/myAccountHomePage';
 import myAccountCreateAccountPage from '../Pages/myAccountCreateAccountPage';
 
+const BASE_URL = 'https://purchase.allstate.com/onlineshopping/welcome';
 
+test.describe('Quote Start Validation', () => {
+    let welcome;
+  
+    test.beforeEach(async ({ page }) => {
+      await page.goto(BASE_URL, { waitUntil: 'load' });
+      welcome = new welcomePage(page);
+    })
 
-/*
--- Commenting out this TC due to flaky issue with allstate.com --
+  test('Should not allow user to start a quote with Renter/Home selected', async () => {
+    await welcome.welcomePageRenterHomeError();
+  })
 
-test('Renters Happy path', async ({ page }) => {
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome')
+  test('Should not allow user to start a quote with Renter/Condo selected', async () => {
+    await welcome.welcomePageRenterCondoError();
+  })
 
-    const welcome = new welcomePage(page)
-    await welcome.welcomePageNavigation()
-    
-    const customerDetails = new enterCustomerDetailsPage(page)
-    await customerDetails.enterHappyPathCustomerDetails()
+  test('Should show user an error when ZIP is not entered', async () => {
+    await welcome.enterYourZipErrorMessage();
+  })
 
-    const creditNoHit = new creditNoHitPage(page)
-    await creditNoHit.validateCreditNoHit()
-
+  test('Should show user an error when no product is selected', async () => {
+    await welcome.welcomePageAtLeaseOneProducterror();
+  })
 })
 
-*/
-
-
-test('Cannot start a quote with Renter/Home', async ({ page }) => {
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome', { waitUntil: 'load' })
-
-    const welcome = new welcomePage(page)
-    await welcome.welcomePageRenterHomeError()
-
-
-})
-
-test('Cannot start a quote with Renter/Condo', async ({ page }) => {
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome', { waitUntil: 'load' })
-
-    const welcome = new welcomePage(page)
-    await welcome.welcomePageRenterCondoError()
-
-
-})
-
-test('Cannot start a quote without entering ZIP', async ({ page }) => {
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome', { waitUntil: 'load' })
-
-    const welcome = new welcomePage(page)
-    await welcome.enterYourZipErrorMessage()
-
-
-})
-
-test('Cannot start a quote without seleting a product', async ({ page }) => {
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome', { waitUntil: 'load' })
-
-    const welcome = new welcomePage(page)
-    await welcome.welcomePageAtLeaseOneProducterror()
-
-
-})
-
-test('MyAccount - Attempt to Login with invalid username and Password - validate Error', async ({ page }) => {
-
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome')
-
-    const welcome = new welcomePage(page)
-    await welcome.myAccountLink()
+test.describe('MyAccount Flow Validation', () => {
+  async function navigateToMyAccountPage(page) {
+    const welcome = new welcomePage(page);
+    await page.goto(BASE_URL);
+    await welcome.myAccountLink();
 
     const [newPage] = await Promise.all([
-        page.waitForEvent("popup") 
+      page.waitForEvent('popup'),
+    ]);
 
-    ]
-    )
+    await newPage.waitForLoadState('domcontentloaded');
+    return newPage;
+  }
 
-    newPage.waitForLoadState("domcontentloaded")
-    const myaccount = new myAccountHomePage(newPage)
-    await myaccount.validateMyAccountLoginError()
+  test('Login with invalid credentials should show error', async ({ page }) => {
+    const newPage = await navigateToMyAccountPage(page);
+    const myaccount = new myAccountHomePage(newPage);
+    await myaccount.validateMyAccountLoginError();
+  })
 
+  test('Register individual account with blank fields should show validation error', async ({ page }) => {
+    const newPage = await navigateToMyAccountPage(page);
+    const myaccount = new myAccountHomePage(newPage);
+    await myaccount.clickRegisterLink();
+
+    const createAccount = new myAccountCreateAccountPage(newPage);
+    await createAccount.validateCreateAccountErrorIndividual();
+  })
+
+  test('Register business account with blank fields should show validation error', async ({ page }) => {
+    const newPage = await navigateToMyAccountPage(page);
+    const myaccount = new myAccountHomePage(newPage);
+    await myaccount.clickRegisterLink();
+
+    const createAccount = new myAccountCreateAccountPage(newPage);
+    await createAccount.validateCreateAccountErrorBusiness();
+  })
 })
-
-
-test('MyAccount - Attempt to Register an indivudal account leaving required fields blank  - Validate Error', async ({page}) =>{
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome')
-
-    const welcome = new welcomePage(page)
-    await welcome.myAccountLink()
-
-    const [newPage] = await Promise.all([
-        page.waitForEvent("popup") 
-
-    ]
-    )
-
-    newPage.waitForLoadState("domcontentloaded")
-    const myaccount = new myAccountHomePage(newPage)
-    await myaccount.clickRegisterLink()
-
-    const createAccount = new myAccountCreateAccountPage(newPage)
-    await createAccount.validateCreateAccountErrorIndividual()
-
-
-})
-
-
-
-test('MyAccount - Attempt to Register an Business account leaving required fields blank  - Validate Error', async ({page}) =>{
-    await page.goto('https://purchase.allstate.com/onlineshopping/welcome')
-
-    const welcome = new welcomePage(page)
-    await welcome.myAccountLink()
-
-    const [newPage] = await Promise.all([
-        page.waitForEvent("popup") 
-
-    ]
-    )
-
-    newPage.waitForLoadState("domcontentloaded")
-    const myaccount = new myAccountHomePage(newPage)
-    await myaccount.clickRegisterLink()
-
-    const createAccount = new myAccountCreateAccountPage(newPage)
-    await createAccount.validateCreateAccountErrorBusiness()
-
-
-})
-
-
