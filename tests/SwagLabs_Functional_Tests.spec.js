41ffa11dd7c020a8354b45a192e30748a1790195
Karim Saidi <keirasaidi@Karims-MacBook-Pro.local>
2025-06-26 20:50:05 +0100

    added high to low validation

--- tests/SwagLabs_Functional_Tests.spec.js
+++ tests/SwagLabs_Functional_Tests.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 import SwagLabs_Login from '../Pages/SwagLabs_Login';
 import SwagLabs_InventoryMenu from '../Pages/SwagLabs_InventoryMenu';
 
-
+/*
 test('Validate Login Error when no username or password is entered and login is selected', async ({ page }) => {
     await page.goto('https://www.saucedemo.com/');
     const login = new SwagLabs_Login(page)
@@ -53,6 +53,39 @@ test('Validate Login Error when no username or password is entered and login is 
   //Uses .sort((a, b) => a - b) to sort the copy in ascending order.
   //compare the original order to a freshly sorted version, without mutating it.
   const sortedPrices = [...prices].sort((a, b) => a - b);
+
+  // Assert original order matches expected sorted order
+  //sserts that the visual order of prices on the page (prices) exactly matches the correct ascending order (sortedPrices).
+  //if they're not the same, the test fails (fileter did not work)
+  expect(prices).toEqual(sortedPrices);
+
+
+  });
+
+  */
+
+  test('Validate Price (high to low) sorting on inventory page', async ({ page }) => {
+    await page.goto('https://www.saucedemo.com/');
+    const login = new SwagLabs_Login(page)
+    await login.validUserLogin()
+
+    const inventoryMenu = new SwagLabs_InventoryMenu(page)
+    await inventoryMenu.validateInvenoryPage()
+
+      // Apply the sort filter: "Price (high to low)"
+  await inventoryMenu.selectSortOption("hilo");
+
+  // Retrieve all item prices displayed
+  //Finds all visible item prices on the page (.inventory_item_price).
+  //Extracts and parses them into an array of numbers (e.g., [29.99, 15.99, 9.99, ...]).
+  //Stores the current visual order of prices after sorting.
+  const prices = await inventoryMenu.getAllItemPrices();
+
+  // Clone and sort a copy to compare against original
+  //Makes a copy of the prices array.
+  //Uses .sort((a, b) => a - b) to sort the copy in ascending order.
+  //compare the original order to a freshly sorted version, without mutating it.
+  const sortedPrices = [...prices].sort((a, b) => a + b);
 
   // Assert original order matches expected sorted order
   //sserts that the visual order of prices on the page (prices) exactly matches the correct ascending order (sortedPrices).
